180b5989e5197bc72e83539af82701c6c30d9fe2
Kamal Kumar <kamal_kumar@epam.com>
2022-07-25 13:49:23 +0530

    init

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  type        = string
+  description = "[Required] Name of the Resource Group."
+}
+
+variable "location" {
+  type        = string
+  description = "[Required] Location of the Resource Group."
+}
